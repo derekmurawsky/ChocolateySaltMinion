cacab6345ffa33556755efd96497489c78ecfbf7
Derek Murawsky <dmurawsky@polarismanagement.com>
2013-11-15 10:21:28 -0500

    Initial seed and working copy.

---
+++ saltminion/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'saltminion'
+$installerType = 'EXE' 
+$url = 'http://docs.saltstack.com/downloads/Salt-Minion-0.17.2-win32-Setup.exe'
+$url64 = 'http://docs.saltstack.com/downloads/Salt-Minion-0.17.2-AMD64-Setup.exe' 
+$silentArgs = '/S' 
+$validExitCodes = @(0) 
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
---
+++ saltminion/tools/chocolateyUninstall.ps1
@@ -0,0 +1,2 @@
+# By default, salt installs in c:\salt This package will attempt to call the uninstall executable from there. 
+C:\salt\uninst.exe /S
